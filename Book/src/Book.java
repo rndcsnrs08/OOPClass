1b34a67e5391c1b23e6ee5d6cc91851dcb6c1abe
rndcsnrs08 <rndcsnrs08@gmail.com>
2017-02-03 13:57:18 -0500

    Add files via upload

---
+++ Book/src/Book.java
@@ -0,0 +1,17 @@
+
+public class Book {
+	private String title;
+	private String author;
+	private static int counter = 0;
+	private int ISBN;
+	
+	public Book (String t, String a){
+		author = a;
+		title = t;
+		ISBN = counter;
+		++counter;
+	}
+	public String getTitle(){return title;}
+	public String getAuthor(){return author;}
+	public int getSerial(){return ISBN;}
+}
---
+++ Book/src/BookCopy.java
@@ -0,0 +1,4 @@
+
+public class BookCopy {
+	String title;
+}
