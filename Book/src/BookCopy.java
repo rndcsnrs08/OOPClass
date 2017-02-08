40afdc486c6e7445ae43304a66d5d09219e9db1d
rndcsnrs08 <rndcsnrs08@gmail.com>
2017-02-08 06:51:43 -0500

    Update BookCopy.java
    created constructor, methods

--- Book/src/BookCopy.java
+++ Book/src/BookCopy.java
@@ -5,5 +5,14 @@ public class BookCopy extends Vector {
 	private Book title;
 	private LibraryCard renter;
 	
-
+	public BookCopy(Book t){
+		title = t;
+		renter = null;
+	}
+	public checkOut(LibraryCard r){
+		renter = r;
+	}
+	public checkIn(){
+		renter = null;
+	}
 }
