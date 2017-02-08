d4f579998a8f390add29c55189099638517f29b5
rndcsnrs08 <rndcsnrs08@gmail.com>
2017-02-08 07:30:39 -0500

    Update Tester.java
    Added to bookList

--- Book/src/Tester.java
+++ Book/src/Tester.java
@@ -4,14 +4,14 @@ public class Tester {
 		
 		List<Vector<BookCopy>> bookList = new ArrayList<BookCopy>();
 		bookList.add(BookCopy catHat = new BookCopy((Book catHat = new Book("Cat hat", "Seuss")), 2);
-		bookList.add(BookCopy lionWitch = new BookCopy(Book lionWitch = new Book("Lion Witch", "Lewis");
-		Book gatsby = new Book("Gatsby", "Fitzgerald");
-		Book wonka = new Book("Wonka", "Dahl");
-		Book greenEggs = new Book("Green Eggs", "Seuss");
-		Book ender = new Book("Ender", "Card");
-		Book quixote = new Book("Quixote", "Dunno");
-		Book math = new Book("Math", "Freund");
-		Book oop = new Book("OOP", "Java");
-		Book tired = new Book("Tired", "Me");
+		bookList.add(BookCopy lionWitch = new BookCopy(Book lionWitch = new Book("Lion Witch", "Lewis")), 2);
+		bookList.add(BookCopy gatsby = new BookCopy(Book gatsby = new Book("Gatsby", "Fitzgerald")), 2);
+		bookList.add(BookCopy wonka = new BookCopy(Book wonka = new Book("Wonka", "Dahl"),2);
+		bookList.add(BookCopy greenEggs = new BookCopy(Book greenEggs = new Book("Green Eggs", "Seuss"),2);
+		bookList.add(BookCopy ender = new BookCopy(Book ender = new Book("Ender", "Card"),2);
+		bookList.add(BookCopy quixoted = new BookCopy(Book quixote = new Book("Quixote", "Dunno"),2);
+		bookList.add(BookCopy math = new BookCopy(Book math = new Book("Math", "Freund"),2);
+		bookList.add(BookCopy oop = new BookCopy(Book oop = new Book("OOP", "Java"),2);
+		bookList.add(BookCopy tired = new BookCopy(Book tired = new Book("Tired", "Me"),2);
 	}
 }
