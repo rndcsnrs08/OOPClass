8eed467e6cbf37734254052fc5731c2a84738586
rndcsnrs08 <rndcsnrs08@gmail.com>
2017-02-08 07:01:17 -0500

    Update Tester.java
    Added List to library

--- Book/src/Tester.java
+++ Book/src/Tester.java
@@ -2,15 +2,16 @@
 public class Tester {
 	public static void main(String[] args){
 		
+		List<BookCopy> bookList = new ArrayList();
 		Book catHat = new Book("Cat hat", "Seuss");
 		Book lionWitch = new Book("Lion Witch", "Lewis");
 		Book gatsby = new Book("Gatsby", "Fitzgerald");
 		Book wonka = new Book("Wonka", "Dahl");
 		Book greenEggs = new Book("Green Eggs", "Seuss");
-		Book catHat = new Book("Cat hat", "Seuss");
-		Book catHat = new Book("Cat hat", "Seuss");
-		Book catHat = new Book("Cat hat", "Seuss");
-		Book catHat = new Book("Cat hat", "Seuss");
-		Book catHat = new Book("Cat hat", "Seuss");
+		Book ender = new Book("Ender", "Card");
+		Book quixote = new Book("Quixote", "Dunno");
+		Book math = new Book("Math", "Freund");
+		Book oop = new Book("OOP", "Java");
+		Book tired = new Book("Tired", "Me");
 	}
 }
