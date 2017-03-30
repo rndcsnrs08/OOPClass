a76d870a19b87fc8d927ec0054a5cb822ddb3041
rndcsnrs08 <rndcsnrs08@gmail.com>
2017-03-30 11:54:35 -0400

    Update LibraryMaterial.java
    Added constructor

--- HW3/LibraryMaterial.java
+++ HW3/LibraryMaterial.java
@@ -2,6 +2,12 @@ public abstract class LibraryMaterial {
 	private String title;
 	private String ISBN;
 	
+	//constructor for LibraryMaterial class; must use super(i,t) in sublcasses
+	public LibraryMaterial(String i, String t){
+		ISBN = i;
+		title = t;
+	}
+	
 	public void print(){
 		System.out.println("Title: " + title + "ISBN: " + ISBN;);
 	}
