c77f0999bf96bcf3e2d67e562ef801148423b67c
rndcsnrs08 <rndcsnrs08@gmail.com>
2017-03-30 12:03:07 -0400

    Update LibraryMaterialCopy.java
    Added abstract methods

--- HW3/LibraryMaterialCopy.java
+++ HW3/LibraryMaterialCopy.java
@@ -2,11 +2,7 @@ public abstract class LibraryMaterialCopy {
 	private LibraryCard userCard;
 	private DueDate userDate;
 	
-	Book getLibraryMaterial(){
-		return userCard.getBook();
-	}
-	
-	DVD getLibraryMaterial(){
-		return userCard.getDVD();
-	}
+	abstract LibraryMaterial getLibraryMaterial();
+	abstract getTitle();
+	abstract getISBN();
 }
