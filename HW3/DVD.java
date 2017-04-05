d4a9601a9b250785a2b4e74273f24fa8cc2c8394
rndcsnrs08 <rndcsnrs08@gmail.com>
2017-04-05 19:51:09 -0400

    Update DVD.java
    Added final comments to DVD

--- HW3/DVD.java
+++ HW3/DVD.java
@@ -7,10 +7,12 @@ class DVD extends LibraryMaterial {
 		mainActor = mA;
 	}
 
+	//accessors for subclass call accessors for superclass if necessary
 	public String getIsbn () {return super.getIsbn();}
 	public String getTitle() {return super.getTitle();}
 	public String getMainActor() {return mainActor;}
 	
+	//print method, calls super first.
 	public void print() {
 		super.print();
 		System.out.println("Main Actor: " + mainActor);
